ef9ff3d19d1d47d5af2dab3a295f73e3c38db36a
Loren <lrondan95@gmail.com>
2019-04-07 19:55:47 +0200

    Arreglos CU-3

--- Acme-Hacker-Rank/src/main/java/repositories/ProblemRepository.java
+++ Acme-Hacker-Rank/src/main/java/repositories/ProblemRepository.java
@@ -11,14 +11,14 @@ import domain.Problem;
 
 @Repository
 public interface ProblemRepository extends JpaRepository<Problem, Integer> {
-
-	@Query("select p from Problem p where p.position.id = ?1")
+	
+	@Query("select p from Problem p join p.positions po where po.id = ?1")
 	Collection<Problem> findAllByPositionId(int positionId);
 
 	@Query("select p from Problem p where p.company.id = ?1")
 	Collection<Problem> findAllByCompanyId(int companyId);
 	
-	@Query("select count(p) from Problem p where p.position.id = ?1")
+	@Query("select count(p) from Problem p join p.positions po where po.id = ?1")
 	Integer countByPositionId(int positionId);
 
 }
